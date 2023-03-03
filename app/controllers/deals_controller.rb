70016a1c15c5de2df956bdf057ee7027f29882a0
Ben Connolly <benconnollyofficial@gmail.com>
2023-03-03 13:04:12 +0000

    Whitelist permitted params

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -3,14 +3,14 @@ class DealsController < ApplicationController
   skip_forgery_protection
 
   def index
-    response = API_CLIENT.get_all_deals_for_city(params[:city])
+    response = API_CLIENT.get_all_deals_for_city(permitted_params[:city])
     @response = JSON.parse(response.to_s)
     @response.each do |deal|
       if deal.has_key?("deal")
         entry = deal.values.first
         entry.symbolize_keys!
         offer = {
-          city: params[:city],
+          city: permitted_params[:city],
           offer_id: entry[:id],
           short_title: entry[:short_title],
           price: entry[:price],
@@ -20,7 +20,7 @@ class DealsController < ApplicationController
         Deal.find_or_create_by(offer)
       end
     end
-    @deals = Deal.select(:short_title, :price, :offer_id, :city).where(city: params[:city])
+    @deals ||= Deal.select(:short_title, :price, :offer_id, :city).where(city: permitted_params[:city])
   end
 
   def home
@@ -33,7 +33,7 @@ class DealsController < ApplicationController
   end
 
   def show
-    @selected_deal = Deal.find_by(offer_id: params[:id])
+    @selected_deal = Deal.find_by(offer_id: permitted_params[:id])
     response = API_CLIENT.get_deal_for_city(@selected_deal.city, @selected_deal.offer_id)
     parsed_response = JSON.parse(response.to_s)
     properties = {
@@ -50,6 +50,6 @@ class DealsController < ApplicationController
   private
 
   def permitted_params
-    params.permit(:city, :type, :authenticity_token, :commit, :deal_id, :format)
+    params.permit(:city, :type, :authenticity_token, :commit, :id, :format)
   end
 end
